01be86269ac60ab5619b785cd1dc3720da1b0273
Ankhaa1173 <82991918+Ankhaa1173@users.noreply.github.com>
2022-10-17 02:22:39 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function redirect(){
+    let table = "";
+    let header = "Ankhbayar Otgonbayar ";
+    let text = "<br>2002 ond tursun buguud 11r dund surguulig tugsuj odoogoor muis iin programm hangamj mergejlin 3r kursin oyutan<br><br>   Uurin gesn yrtuntsiig buteeh buyu togloom hiih zorilgoor ene mergejlee songoson"
+    table = table + "<div class = 'header'>"+header+"<div class = 'text'>"+text+"</div></div>";
+    document.getElementById("text").innerHTML = table;
+    document.getElementById("text").style.opacity = 1;   
+}
+function redirect1(){
+    let table = "";
+    let header = "Goal";
+    let text = "<br>Mihoyo gedeg company 2030 on gehed 1 terbum hun orj boldg togloom buteeh zorilgotoi buguud ene company-d ajilaj ulmar uurin gsn software hugjuuldg ter dotroo togloom golchlon buteedg company tai bolno"
+    table = table + "<div class = 'header'>"+header+"<div class = 'text'>"+text+"</div></div>";
+    document.getElementById("text").innerHTML = table;
+    document.getElementById("text").style.opacity = 1;   
+}
+function redirect2(){
+    let table = "";
+    let header = "Skill & HOBBY";
+    let text = "<br>Chuluut tsagaaraa medeej video game toglodog mun sport(sags) togloh durtai<br><br> Muis-d elsen orsnooso hoish HTML,CSS,Javascript,C/C++, Java helnuudig sursaar yuvaa. Odoogoor Web design develop hiih hamgiin hugjiltei baigaa(haritsangui amarhan sanagdj baigaa uchraas). <br><br> Tsaashdaa C, java ashiglan interactive app, software hiij surahig husej baina"
+    table = table + "<div class = 'header'>"+header+"<div class = 'text'>"+text+"</div></div>";
+    document.getElementById("text").innerHTML = table;
+    document.getElementById("text").style.opacity = 1;   
+}
+function redirect3(){
+    let table = "";
+    let header = "Holboo barih";
+    let text = "<br>utas: 80695408 <br><br> email: ankhaaa1173@gmail.com"
+    table = table + "<div class = 'header'>"+header+"<div class = 'text'>"+text+"</div></div>";
+    document.getElementById("text").innerHTML = table;
+    document.getElementById("text").style.opacity = 1;   
+}
